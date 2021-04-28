f711feb82cfd4fd4666d0c1a02367178bcfdcd41
Fachrul Mustofa <46750697+Fachrulmustofa20@users.noreply.github.com>
2021-04-28 23:16:53 +0700

    change mode ip address

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
-        host: process.env.NODE_ENV !== 'production' ? 'localhost' : 'https://note-apps-api.netlify.app/',
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : 'http://apps-note.herokuapp.com/',
         routes: {
             cors: {
                 origin: ['*'],
