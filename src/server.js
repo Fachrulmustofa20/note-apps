c035cd67826fdb8f3f1e9574cccdb4218c66451f
Fachrul Mustofa <46750697+Fachrulmustofa20@users.noreply.github.com>
2021-04-28 23:56:44 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
-        host: process.env.NODE_ENV !== 'production' ? 'localhost' : 'https://note-apps.vercel.app/',
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '112.215.242.55',
         routes: {
             cors: {
                 origin: ['*'],
